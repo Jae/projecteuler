614979050cf7ffd38ee47a565c0674098bbe0565
Jae <jlee@yetitrails.com>
2011-06-22 23:46:06 +0100

    turns out you don't need to export function to be invoked via escript

--- 1/solution.erl
+++ 1/solution.erl
@@ -1,8 +1,8 @@
 -module (solution).
--export ([sum/1]).
 
 main([Max]) ->
-  io:format("sum of product of 3 or 5 below ~p is ~p ~n", [Max, sum(list_to_integer(Max))]).
+  UpperLimit = list_to_integer(Max),
+  io:format("sum of product of 3 or 5 below ~p is ~p ~n", [UpperLimit, sum(UpperLimit)]).
 
-sum(Max) ->
-  lists:sum([X|| X <- lists:seq(1, Max-1), (X rem 3 == 0) or (X rem 5 == 0)]).
+sum(UpperLimit) ->
+  lists:sum([X|| X <- lists:seq(1, UpperLimit-1), (X rem 3 == 0) or (X rem 5 == 0)]).
