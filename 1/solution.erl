f628d42de9a582337555bf7e03e3ceff3aec4fb7
Jae <jlee@yetitrails.com>
2011-06-22 14:02:13 +0100

    solution1

--- 1/solution.erl
+++ 1/solution.erl
@@ -1,7 +1,8 @@
 -module (solution).
 -export ([sum/1]).
 
+main([Max]) ->
+  io:format("sum of product of 3 or 5 below ~p is ~p ~n", [Max, sum(list_to_integer(Max))]).
+
 sum(Max) ->
-  List = [X|| X <- lists:seq(1, Max-1), (X rem 3 == 0) or (X rem 5 == 0)],
-  lists:sum(List).
-  
+  lists:sum([X|| X <- lists:seq(1, Max-1), (X rem 3 == 0) or (X rem 5 == 0)]).
